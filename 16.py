d9bcbc715a38bddecaa9cffc13d32b5f962818b4
jg_craptop <jasongalea98@outlook.com>
2020-01-29 16:46:20 +1100

    Finished puzzle 16

--- 16.py
+++ 16.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 from functions import *
 
+n = int(input("\nEnter n: "))
+digits = 2**n
+result = 0
+
+for x in str(digits):
+    result += int(x)
+
+print("\nThe sum of the digits in 2^%s or %s, equals %s\n" %(n, digits, result))
