3600161a8f1676b14cc8cad978b846db577df625
jg_craptop <jasongalea98@outlook.com>
2020-01-29 13:49:48 +1100

    Clean up + work on 15

--- 15.py
+++ 15.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 from functions import *
 
-size = input("Enter a number for the size of the array:\n")
-print(size)
+size = int(input("\nEnter a number for the size of the square array:\n")) + 1
+arr = [[0] * size] * size
+
+
+
+print("Printing nested array to show ")
+for row in arr:
+    print(row)
+print()
