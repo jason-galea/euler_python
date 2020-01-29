9a6f2ffd03668c2c66de2ad0458241fcc04e2e83
jg_craptop <jasongalea98@outlook.com>
2020-01-29 15:49:34 +1100

    Starting 15 from scratch

--- 15.py
+++ 15.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 from functions import *
 
-size = int(input("\nEnter a number for the size of the square array:\n")) + 1
+# Recieve size of array 
+size = int(input("\nEnter the size of the square array:\n")) + 1
+    # +1 because this array represents vertices, not the literal square
+
+# Create empty array
 arr = [[0] * size] * size
 
+# Set bottom and left edges all to "1", due to movement restrictions
+arr[size - 1] = [1] * size
+for x in arr:
+    x[size - 1] = 1
 
 
-print("Printing nested array to show ")
-for row in arr:
-    print(row)
-print()
+
+
+# Print completed grid, answer is at the top-left corner
+for x in arr:
+    print(x)
