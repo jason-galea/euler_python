a4e29ed5de0b2bb250ada3698ea8bacaa4fd9e4c
Jason Galea <jasonpgalea@outlook.com.au>
2021-12-08 16:52:34 +1100

    Restart 24, smarter

--- 24.py
+++ 24.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-
-
 # A permutation is an ordered arrangement of objects.
 # For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
 # If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.
@@ -10,35 +7,78 @@
 
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
-def getLPRecursive():
-    pass
+def getLPRecursive(inputArray): # Expects ordered array of n digits
+    result = []
+    for i in inputArray: # [0,1,2]
+        iDigitBank = inputArray.copy()
+
+        iOutput = []
+        iOutput.append(i) # Potential outputs for this iteration = [0,x,x]
+        iDigitBank.remove(i) # [0,1,2] --> [1,2]
+
+        for j in iDigitBank: # [1,2]
+            jDigitBank = iDigitBank.copy()
+
+            jOutput = iOutput.copy()
+            jOutput.append(j) # Potential outputs for this iteration = [0,1,x]
+            jDigitBank.remove(j) # [1,2] --> [2]
+
+            for k in jDigitBank: # [2]
+                kDigitBank = jDigitBank.copy()
+
+                kOutput = jOutput.copy()
+
+                if (len(jDigitBank) == 1): # Redundant, illistrative
+                    kOutput.append(k)
+                    result.append(kOutput)
+
+                # kOutput.append(k) # Potential outputs for this iteration = [0,1,2]
+                # kDigitBank.remove(k) # [2] --> []
+
+
+
+    return result
 
 def getLPStatic(inputArray): # Expects ordered array of 3 digits
     result = []
-    for i, iDigit in enumerate(inputArray):
-        iArray = inputArray.copy() # Local duplicate of [0,1,2]
+    for i in inputArray: # [0,1,2]
+        iDigitBank = inputArray.copy()
+
         iOutput = []
-        iOutput.append(iArray.pop(i)) # Stem of new branch [0,?,?]
+        iOutput.append(i) # Potential outputs for this iteration = [0,x,x]
+        iDigitBank.remove(i) # [0,1,2] --> [1,2]
 
-        for j, jDigit in enumerate(iOutput):
-            jArray = iArray.copy() # Local duplicate of [1,2]
+        for j in iDigitBank: # [1,2]
+            jDigitBank = iDigitBank.copy()
+
             jOutput = iOutput.copy()
-            jOutput.append(jArray.pop(j)) # Stem of new branch [0,1,?]
+            jOutput.append(j) # Potential outputs for this iteration = [0,1,x]
+            jDigitBank.remove(j) # [1,2] --> [2]
 
-            for k, kDigit in enumerate(jOutput):
-                kArray = jArray.copy() # Local duplicate of [2]
+            for k in jDigitBank: # [2]
+                kDigitBank = jDigitBank.copy()
+
                 kOutput = jOutput.copy()
-                kOutput.append(kArray.pop(k))
-                
-                result.append(kOutput)
+
+                if (len(jDigitBank) == 1): # Redundant, illistrative
+                    kOutput.append(k)
+                    result.append(kOutput)
+
+                # kOutput.append(k) # Potential outputs for this iteration = [0,1,2]
+                # kDigitBank.remove(k) # [2] --> []
+
+
 
     return result
 
 def main():
     result = getLPStatic([0,1,2])
+    print(result)
+    # for s in result:
+        # print(s)
+
     # result = getLPRecursive([0,1,2,3,4,5,6,7,8,9])
-    for s in result:
-        print(s)
+    # print(result[1000000])
 
 if __name__ == "__main__":
     main()
