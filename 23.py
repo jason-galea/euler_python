956d427549cdb807e9458f54c850b813ab2ee067
Jason Galea <jasonpgalea@outlook.com>
2021-12-09 21:09:10 +1100

    TWENTY THREE. BRUTE FORCE ME

--- 23.py
+++ 23.py
@@ -1,37 +1,38 @@
-#!/usr/bin/env python3
 
-def sum_of_divisors(n): # Find all divisors THEN sum
-    divisors = [1]
+def sumOfDivisors(n):
+    sumOfDivisors = 1
     for i in range(2, int(n/2) + 1): # Find divisors
         if (n % i == 0):
-            divisors.append(i)
-    return sum(divisors)
+            sumOfDivisors += i
+    return sumOfDivisors
 
 def main():
-    # Get all abundant numbers
+    ### Get all abundant numbers
     abundants = []
-    # abundantHashes = []
-    for i in range(12, 28124):
-        if (i < sum_of_divisors(i)):
+    for i in range(12, 28124, 2):
+        if (i < sumOfDivisors(i)):
             abundants.append(i)
-            # abundantHashes.append(hash(i))
+            print(f"Found adundant number {i}")
+    for i in range(15, 28124, 10):
+        if (i < sumOfDivisors(i)):
+            abundants.append(i)
+            print(f"Found adundant number {i}")
     print(f"Found {len(abundants)} abundant numbers")
 
-    # For all positive integers, check if they can NOT be written as a sum of two abundants
-    posIntsNotExpressibleAsTwoAbundants = []
-    for i in range(1, 28124):
-        iExpressibleAsTwoAbundants = False # Not yet known
-        for abundant in abundants:
-            if (i - abundant in abundants): # If it IS able to be written as two, break loop
-                iExpressibleAsTwoAbundants = True
-                break
-            # if (hash(i - abundant) not in abundantHashes):
-            #     break
-        if (not iExpressibleAsTwoAbundants): # Then "i" made it through the loop without failure
-            posIntsNotExpressibleAsTwoAbundants.append(i)
-
-    
-    print(f"Result: {sum(posIntsNotExpressibleAsTwoAbundants)}")
+    ### For every sum of abundants, remove sum from set of all posInts
+    posInts = list(range(1, 28124))
+    lenAbundants = len(abundants)
+    for i, aa in enumerate(abundants):
+        for j in range(i, lenAbundants):
+            ab = abundants[j]
+            sum = aa + ab
+            # if (sum in posInts):
+            try:
+                posInts.remove(sum)
+                print(f"Removed {aa} + {ab} = {sum} from posInts[]")
+            except:
+                pass
+    print(f"Result: {sum(posInts)}")
 
 if __name__ == "__main__":
     main()
