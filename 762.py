9cfc37841ff5e74b8cb09bcfed7974c020315a3a
Jason Galea <jasonpgalea@outlook.com.au>
2021-12-10 22:17:12 +1100

    762

--- 762.py
+++ 762.py
@@ -1,34 +1,40 @@
 # https://projecteuler.net/problem=762
 # https://www.mail-archive.com/chat@forums.jsoftware.com/msg03978.html
 
-class Grid:
-    def __init__(self) -> None:
-        self.grid = [ #TODO: Compress
-            [],
-            [],
-            [],
-            [True]
-        ]
-        self.possibleGrids = [] # Including duplicates
-        self.uniqueGrids = 0
 
-    def getIterations(self, n): # Generate all possible iterations for C(n)
-        pass
-        # Generate
+def getIterations(n): # Generate all possible iterations for C(n)
+    pass
+    # Generate
 
-    def saveIterations(self):
-        pass
+def saveIterations():
+    pass
 
-    def areMatchingGrids(self, g1, g2):
+def areMatchingGrids(g1, g2):
+    pass
+
+        
+
+
+def main(limit):
+    grid = [ # Inefficient yet verbose data structure, will compress for later comparison
+        [],
+        [],
+        [],
+        [True]
+    ]
+    possibleGrids = [] # Including duplicates
+    uniqueGrids = 0
+
+    while (limit > 0):
         pass
 
         
 
 
 
-def main():
-    myGrid = Grid()
-    myGrid.getIterations
-
 if __name__ == "__main__":
-    main()
+    main(2)
+    # main(10)
+    # main(20)
+    # main(100)
+    # main(100000)
