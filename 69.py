1ba37c5a6cec323ce12ee4626d68af12a6e4de84
Jason Galea <jasonpgalea@outlook.com>
2022-10-31 14:47:58 +1100

    Move work on 69

--- 69.py
+++ 69.py
@@ -1,5 +1,25 @@
-def main():
-    pass
+def n_over_totient_n(n):
+    return n/totient(n)
+
+def totient(n):
+
+    # relative_primes = set()
+    relative_primes = {1}
+    
+    # iterate over 1 --> n - 1
+    for i in range(2, n + 1):
+        if (n % i == 0):
+            relative_primes.add(i)
+
+    print(f"relative_primes of {n}: {relative_primes}")
+
+    return len(relative_primes)
+
+
+def find_maximum(max_n):
+    current_maximum = 0
+
+    for i in range(2, max_n + 1):
 
 if __name__ == "__main__":
-    main()
+    find_maximum(10)
