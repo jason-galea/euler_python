e98790c951a70f2972171dcf704259bd9d5da3f0
Jason Galea <jasonpgalea@outlook.com>
2022-10-31 13:21:53 +1100

    28 done

--- 28.py
+++ 28.py
@@ -2,13 +2,18 @@ def find_sum_of_diagonals(max_grid_size):
     """
     Expects an odd number
     """
+
+    DEBUG = True
+    # DEBUG = False
     
     sum = 1
     current_grid_size = 1
     previous_number = 1
+    if DEBUG: print(f"previous_number = {previous_number}")
 
     while True:
         current_grid_size += 2
+        if DEBUG:  print(f"current_grid_size = {current_grid_size}")
 
         ### Find & add diagonals from current iteration
         ### NOTE: Increment between diagonals to be added == current_grid_size -1
@@ -16,11 +21,12 @@ def find_sum_of_diagonals(max_grid_size):
         ### NOTE: Increment == 25 - 21 == 4 == current_grid_size - 1
         for _ in range(4):
             previous_number += current_grid_size - 1
+            if DEBUG:  print(f"previous_number = {previous_number}")
 
             sum +=  previous_number
 
 
-        if (current_grid_size == max_grid_size):
+        if (current_grid_size >= max_grid_size):
             # return sum
             print(f"Sum of diagonal numbers in grid of size {max_grid_size} == {sum}")
             exit()
@@ -30,6 +36,7 @@ def find_sum_of_diagonals(max_grid_size):
         
 
 if __name__ == "__main__":
-    find_sum_of_diagonals(3)
+    # find_sum_of_diagonals(3)
     # find_sum_of_diagonals(5) ### Should return 101
-    # find_sum_of_diagonals(1001)
+    # find_sum_of_diagonals(101)
+    find_sum_of_diagonals(1001)
