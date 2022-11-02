bdd35c33025a8f837b581f3a385c7e8965b7bcc6
Jason Galea <jasonpgalea@outlook.com.au>
2022-11-03 00:25:23 +1100

    69, stuck in the weeds

--- 69.py
+++ 69.py
@@ -1,13 +1,13 @@
 DEBUG = False
 
 def is_a_factor(potential_factor, i):
-    # return (i % potential_factor == 0)
+    return (i % potential_factor == 0)
 
-    result = (i % potential_factor == 0)
-    if (result):
-        print(f"{potential_factor} is a factor of {i}")
+    # result = (i % potential_factor == 0)
+    # if (result):
+    #     print(f"\t\t\t\t{potential_factor} is a factor of {i}")
 
-    return result
+    # return result
 
 def sqrt(i):
 	return i**0.5
@@ -30,29 +30,36 @@ def yield_primes(i):	### Accepts int, yields all primes below and including 'i'
 				arr[y] = False
 
 def is_coprime(a, b): ### Assumes a < b
-    # print("")
+    # print(f"\t\tis_coprime({a}, {b})")
 
     ### If ANY common factors, return False
-    ### NOTE: "i" ONLY fails when i is prime
-    ### NOTE: So there's no point checking for non-prime factors
 
     for i in range(2, a + 1): ### Naive
+    # for i in range(3, a + 1, 2): ### Odd numbers only
+    # for i in range(2, a + 1, 2): ### Even numbers only
     # for i in yield_primes(a): ### Very high overhead :/
 
     # ### Don't check range above sqrt(a) + 1
     # ### NOTE: This fails, because you can't skip values (why though...)
     # for i in range(2, int(sqrt(a)) + 1):
 
+        # ### Quick and easy fail conditions
+        # if (i > 7):
+        #     # if is_a_factor(2, i): continue ### Already done in range()
+        #     if is_a_factor(3, i): continue
+        #     if is_a_factor(5, i): continue
+        #     if is_a_factor(7, i): continue
+
         # if (a % i == 0) and (b % i == 0):
-        if (is_a_factor(i, a)) and (is_a_factor(i, b)):
-            # print(f"is_coprime({a}, {b}) == False")
-            print(f"{i} IS a factor of {a} and {b}")
+        if is_a_factor(i, a) and is_a_factor(i, b):
+            # print(f"\t\t\t{i} IS a factor of {a} and {b}")
+            print(f"\t\t\tis_coprime({a}, {b}) == False")
             return False
 
         # else:
-        #     print(f"{i} IS NOT a factor of {a} and {b}")
+        #     print(f"\t\t\t{i} IS NOT a factor of {a} and {b}")
 
-    # print(f"is_coprime({a}, {b}) == True")
+    print(f"\t\t\tis_coprime({a}, {b}) == True")
     return True
 
 def totient(n, debug=DEBUG):
@@ -63,7 +70,7 @@ def totient(n, debug=DEBUG):
         if (is_coprime(i, n)):
             relative_primes.add(i)
 
-    if (debug): print(f"(n = {n}): relative_primes = {relative_primes}")
+    # if (debug): print(f"\t(n = {n}): relative_primes = {relative_primes}")
 
     return len(relative_primes)
 
@@ -94,7 +101,7 @@ def main(limit):
             result = n
             max_n_on_tn = current_n_on_tn
 
-            # print(f"(n = {n}) max_n_on_tn = {max_n_on_tn}")
+            print(f"(n = {n}) max_n_on_tn = {max_n_on_tn}")
             # totient(n, debug=True)
 
     print(f"\nFor n ≤ {limit}, n/φ(n) produces a maximum at n = {result}")
