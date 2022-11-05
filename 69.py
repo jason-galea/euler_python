a0ca4ebb1d61078116837d4225ade087e6e67958
Jason Galea <jasonpgalea@outlook.com.au>
2022-11-05 19:52:56 +1100

    Incremental improvements

--- 69.py
+++ 69.py
@@ -1,124 +1,130 @@
-DEBUG = False
+def yield_primes(i):    ### Accepts int, yields all primes below and including 'i'
+    arr = [True] * (i + 1)
 
-def is_a_factor(potential_factor, i):
-    return (i % potential_factor == 0)
+    for x in range(2, i + 1):
+        if arr[x]:
+            yield x
 
-    # result = (i % potential_factor == 0)
-    # if (result):
-    #     print(f"\t\t\t\t{potential_factor} is a factor of {i}")
+            for y in range(x, i + 1, x):
+                arr[y] = False
 
-    # return result
+### TODO: Collapes is_coprime() and factors() into one function 
+def factors(i):        # Accepts int, yields all factors of 'i', including 1 and 'i'
+    result = []
+    sqrt_i = i**0.5 ### Could be a float
 
-def sqrt(i):
-	return i**0.5
-	
-def is_prime(i):
-	for x in range(2, int(sqrt(i)) + 1):
-		# if (i % x == 0):
-		if (is_a_factor(x, i)):
-			return False
+    # for x in range(1, int(sqrt_i + 1)): ### Correct behaviour lol
+    for x in range(2, int(sqrt_i + 1)): ### Ignore 1
+        if (i % x == 0):
 
-	return True
+            result.append(x)
 
-def yield_primes(i):	### Accepts int, yields all primes below and including 'i'
-	arr = [True] * (i + 1)
-	for x in range(2,i + 1):
-		if arr[x]:
-			yield x
+            if (x != sqrt_i): ### Only add matching factor if not square root
+                result.append(i / x)
 
-			for y in range(x,i + 1,x):
-				arr[y] = False
+    result.append(i)
+    return result
 
-def is_coprime(a, b): ### Assumes a < b
-    # print(f"\t\tis_coprime({a}, {b})")
+### TODO: Collapes is_coprime() and factors() into one function 
+def is_coprime(a, b): ### ### If ANY common factors, return False (Assumes a < b)
+    # print(f"==> \tis_coprime({a}, {b})")
 
-    ### If ANY common factors, return False
+    ### Early pass conditions
+    if (abs(a - b) == 1): return True ### Consecutive numbers are always coprime
 
-    for i in range(2, a + 1): ### Naive
-    # for i in range(3, a + 1, 2): ### Odd numbers only
-    # for i in range(2, a + 1, 2): ### Even numbers only
-    # for i in yield_primes(a): ### Very high overhead :/
+    factors_of_a = factors(a)
+    # print(f"\t\tFactors of {a} = {factors_of_a}")
+    if (len(factors_of_a) == 0): return True ### Ignore if prime
 
-    # ### Don't check range above sqrt(a) + 1
-    # ### NOTE: This fails, because you can't skip values (why though...)
-    # for i in range(2, int(sqrt(a)) + 1):
+    for i in factors_of_a: ### Factors of A (ignoring 1)
 
-        # ### Quick and easy fail conditions
-        # if (i > 7):
-        #     # if is_a_factor(2, i): continue ### Already done in range()
-        #     if is_a_factor(3, i): continue
-        #     if is_a_factor(5, i): continue
-        #     if is_a_factor(7, i): continue
+        # print(f"\t\t\tIs {i} a factor of both {a} and {b}?")
 
-        # if (a % i == 0) and (b % i == 0):
-        if is_a_factor(i, a) and is_a_factor(i, b):
-            # print(f"\t\t\t{i} IS a factor of {a} and {b}")
-            print(f"\t\t\tis_coprime({a}, {b}) == False")
+        if (b % i == 0):
+            # print(f"\t\t\tFalse (common factor {i})")
             return False
 
-        # else:
-        #     print(f"\t\t\t{i} IS NOT a factor of {a} and {b}")
 
-    print(f"\t\t\tis_coprime({a}, {b}) == True")
+    # print(f"\t\t\tis_coprime({a}, {b}) == True")
+    # print("\t\t\tTrue")
     return True
 
-def totient(n, debug=DEBUG):
-
-    relative_primes = {1}
-
-    for i in range(2, n): ### Naive
-        if (is_coprime(i, n)):
-            relative_primes.add(i)
-
-    # if (debug): print(f"\t(n = {n}): relative_primes = {relative_primes}")
-
-    return len(relative_primes)
-
 def main(limit):
-    ### Find N that produces highest n/φ(n)
-    ### NOTE: AKA: N with most factors at lowest value
+    """
+    Find N that produces highest n/φ(n)
+    AKA, N with most factors and lowest value
+    """
 
-    ### TODO: Target N will never be prime
-    ### DONE: Target N will never be odd
+    RESULT = 0
+    global MAX_N_ON_TN
+    MAX_N_ON_TN = 0
 
-    result = 0
-    max_n_on_tn = 0
+    
+    ####################################################################
+    ### BEGIN MAIN LOOP (n)
+    # for n in range(2, limit + 1): ### Naive, all numbers
+    # for n in range(2, limit + 1, 2): ### Even numbers only (Target N will never be odd/prime)
+    # for n in range(10, limit + 1, 10): ### Kind of cheating, incrementing based on previous answers
+    for n in range(30, limit + 1, 30): ### Kind of cheating, incrementing based on previous answers
 
-    # for n in range(2, limit + 1): ### Naive
-    for n in range(2, limit + 1, 2):
-    # for n in range(10, limit + 1, 10): ### Breaks if (limit < 30)
+        # print(f"\n(n = {n})")
 
-        print(f"\n(n = {n})")
+        ### Get primes of >= n
+        # primes_including_n = [i for i in yield_primes(n)]
         
-        if (is_prime(n)): continue ### TODO
-        # if (n % 3 != 0): continue ### This is very janky
-        # if (not is_a_factor(3, n)): continue ### This is very janky
         
-        current_n_on_tn = n/totient(n)
-        print(f"(n = {n}) current_n_on_tn = {current_n_on_tn}")
+        ####################################################################
+        ### Find n/φ(n) 
+        # print(f"==> totient({n})")
 
-        if (current_n_on_tn > max_n_on_tn):
-            result = n
-            max_n_on_tn = current_n_on_tn
+        # relative_primes = {1}
+        totient = 1
 
-            print(f"(n = {n}) max_n_on_tn = {max_n_on_tn}")
-            # totient(n, debug=True)
+        # for i in range(2, n): ### Naive, all numbers
+        for i in range(3, n, 2): ### Odd numbers only (coprimes can never be even)
 
-    print(f"\nFor n ≤ {limit}, n/φ(n) produces a maximum at n = {result}")
-    print(f"For n = {result}, n/φ(n) = {max_n_on_tn}")
+            if (is_coprime(i, n)):
+                # relative_primes.add(i)
+                totient += 1
+
+                current_n_on_tn = n / totient
+                # print(f"==> totient({n}): n/φ(n) for current iteration = {current_n_on_tn}")
+
+                if (current_n_on_tn < MAX_N_ON_TN):
+                    # print(f"==> totient({n}): n/φ(n) fell below MAX_N_ON_TN ({MAX_N_ON_TN})")
+                    break
+
+        # print(f"==> totient({n}): Relative primes = {relative_primes}")
+        ####################################################################
+
+        # current_n_on_tn = n/totient(n)
+        # print(f"(n = {n}) current_n_on_tn = {current_n_on_tn}")
+
+        if (current_n_on_tn > MAX_N_ON_TN):
+            RESULT = n
+            MAX_N_ON_TN = current_n_on_tn
+
+            print(f"\n==> (n = {n}) New highest n/φ(n) found: {MAX_N_ON_TN}")
+    
+    
+    ### END MAIN LOOP (n)
+    ####################################################################
+
+    print(f"\n==> n = {RESULT} produces a maximum n/φ(n) ({MAX_N_ON_TN}) for n ≤ {limit}.")
 
 if __name__ == "__main__":
 
     ### Best times!!!
-    main(10)        ### 0m00.013s
-    # main(100)       ### 0m00.013s
-    # main(500)       ### 0m00.018s
-    # main(1000)      ### 0m00.053s
-    # main(5000)      ### 0m4.871s
-    # main(10000)     ### 0m39.498s
-    # main(50000)     ### 
-    # main(100000)    ### 
-    # main(500000)    ### 
+    # main(10)        ### 0m00.013s           ### Answer = 6
+    # main(50)        ### 0m00.013s           ### Answer = 30
+    # main(100)       ### 0m00.013s           ### Answer = 30
+    # main(500)       ### 0m00.018s           ### Answer = 210
+    # main(1000)      ### 0m00.028s           ### Answer = 210
+    # main(5000)      ### 0m00.558           ### Answer = 2310
+    main(10000)     ### 0m02.565s           ### Answer = 2310
+    # main(50000)     ### 1m54.140s           ### Answer = 30030
+    # main(100000)    ### 0m??.???s           ### Answer = ???
+    # main(500000)    ### 0m??.???s           ### Answer = ???
 
     ### GOAL
-    # main(1000000)   ### 
+    # main(1000000)   ### 0m??.???s           ### Answer = ???
--- _functions.py
+++ _functions.py
@@ -1,62 +1,70 @@
 
 def reverseString(s):
-	return ''.join(reversed(s))
+    return ''.join(reversed(s))
 
-def sqrt(i):
-	# return i**(1/2)
-	return i**0.5
-	
+# def sqrt(i):
+#     # return i**(1/2)
+#     return i**0.5
+    
 def isPrime(i):
-	for x in range(2, int(sqrt(i)) + 1):
-		if i % x == 0:
-			return False
-	return True
+    for x in range(2, int(i**0.5) + 1):
+        if (i % x == 0):
+            return False
 
-def yieldPrimes(i):	### Accepts int, yields all primes below and including 'i'
-	arr = [True] * (i + 1)
-	for x in range(2,i + 1):
-		if arr[x]:
-			yield x
+    return True
 
-			for y in range(x,i + 1,x):
-				arr[y] = False
+def yieldPrimes(i):    ### Accepts int, yields all primes below and including 'i'
+    arr = [True] * (i + 1)
 
-def yieldTriangles(i):	# Accepts int, yields all triangle numbers below and including 'i'
-	result = 0
-	for x in range(0, i):
-		result += x
-		if result <= i:
-			yield result
+    for x in range(2, i + 1):
+        if arr[x]:
+            yield x
 
-def factors(i):		# Accepts int, yields all factors of 'i', including 1 and 'i'
-	result = []
-	for x in range(1, int(sqrt(i) + 1)):
-		if i % x == 0:
-			if x != sqrt(i):
-				result.append(x)
-			result.append(int(i / x))
-	result.sort()
+            for y in range(x, i + 1, x):
+                arr[y] = False
 
-	for x in result:
-		yield x
+def yieldTriangles(i):    # Accepts int, yields all triangle numbers below and including 'i'
+    result = 0
 
-def collatz(i):		# Accepts int, yields collatz sequence starting from 'i'
-	yield i
-	while True:
-		if i % 2 == 0:
-			i /= 2
-		else:
-			i = (i * 3) + 1
-		yield int(i)
+    for x in range(0, i):
+        result += x
+        if result <= i:
+            yield result
 
-		if i == 1:
-			break
+def factors(i):        # Accepts int, yields all factors of 'i', including 1 and 'i'
+    result = []
+    sqrt_i = i**0.5 ### Could be a float
 
-def factorial(i):	# Accepts int, returns int!
-	if i == 0:
-		return 1
+    for x in range(1, int(sqrt_i + 1)):
+        if (i % x == 0):
 
-	for x in range(i - 1, 1, -1):
-		i *= x
+            result.append(x)
 
-	return i
+            if (x != sqrt_i): ### Only add matching factor if not square root
+                result.append(int(i / x))
+
+    result.sort()
+
+    for x in result:
+        yield x
+
+def collatz(i):        # Accepts int, yields collatz sequence starting from 'i'
+    yield i
+    while True:
+        if i % 2 == 0:
+            i /= 2
+        else:
+            i = (i * 3) + 1
+        yield int(i)
+
+        if i == 1:
+            break
+
+def factorial(i):    # Accepts int, returns int!
+    if i == 0:
+        return 1
+
+    for x in range(i - 1, 1, -1):
+        i *= x
+
+    return i
