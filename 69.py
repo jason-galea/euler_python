90dd4c94a8a3d36356e5f43c783aaf2d31416d52
Jason Galea <jasonpgalea@outlook.com>
2022-11-06 23:18:07 +1100

    Labeled each is_coprime function, debugging

--- 69.py
+++ 69.py
@@ -1,20 +1,41 @@
-# from functools import cache
+def factors(i): ### Accepts int, returns set of factors (excluding 1 and i)
 
-def yield_primes(i):    ### Accepts int, yields all primes below and including 'i'
-    arr = [True] * (i + 1)
+    # print(f"Finding factors of {i}")
+    # set_of_factors = []
+    set_of_factors = set()
+    sqrt_i = int(i**0.5) ### Ignore floating point
 
-    for x in range(2, i + 1):
-        if arr[x]:
-            yield x
+    for j in range(2, sqrt_i + 1): ### Ignore 1
+        if (i % j == 0):
 
-            for y in range(x, i + 1, x):
-                arr[y] = False
+            # print(f"Found factor of {i}: {j}")
+            # set_of_factors.append(j)
+            set_of_factors.add(j)
 
-def is_coprime_old(a, b): ### If ANY common factors, return False (Assumes a < b)
+            if (j != sqrt_i): ### Only add matching factor if not square root
+                # print(f"Found factor of {i}: {j}")
+                # set_of_factors.append(int(i / j))
+                set_of_factors.add(int(i / j))
+
+    # print(f"Factors of {i} = {set_of_factors}")
+    return set_of_factors
+
+def is_coprime_find_factors_first(i, n):
+    if (abs(i - n) == 1): return True ### Consecutive numbers are always coprime
+    if (n % i == 0): return False ### Account for not including 'a' as a factor of itself below
+
+    factors_of_i = factors(i)
+    # print(f"Factors of {i} = {factors_of_i}")
+
+    for factor in factors_of_i:
+        if (n % factor == 0):
+            return False
+
+    return True
+
+def is_coprime_find_and_check_factors(a, b): ### If ANY common factors, return False (Assumes a < b)
     # print(f"==> \tis_coprime({a}, {b})")
-    # print(f"==> \t Are {a} & {b} coprime?")
 
-    ### Early exits
     if (abs(a - b) == 1): return True ### Consecutive numbers are always coprime
     if (b % a == 0): return False ### Account for not including 'a' as a factor of itself below
 
@@ -38,9 +59,7 @@ def is_coprime_old(a, b): ### If ANY common factors, return False (Assumes a < b
     # print(f"==> \t\t\t {a} & {b} ARE coprime (No common factors found)")
     return True
 
-def is_coprime_old_v2(i, factors_of_n, n=1): ### Returns True if 'i' is cleanly divisible by any factor in given set
-    
-    ### Early exits
+def is_coprime_precompute_factors_of_n(i, factors_of_n, n): ### Returns True if 'i' is cleanly divisible by any factor in given set
     if (abs(i - n) == 1): return True ### Consecutive numbers are always coprime
     if (n % i == 0): return False ### Account for not including 'a' as a factor of itself below
 
@@ -50,12 +69,9 @@ def is_coprime_old_v2(i, factors_of_n, n=1): ### Returns True if 'i' is cleanly 
 
     return True
 
-def is_coprime(i, n): ### Returns True if 'i' is cleanly divisible by any factor in given set
-    
-    ### Early exits
+def is_coprime_precompute_all_factors(i, n): ### Returns True if 'i' is cleanly divisible by any factor in given set
     if (abs(i - n) == 1): return True ### Consecutive numbers are always coprime
     if (n % i == 0): return False ### Account for not including 'a' as a factor of itself below
-
 
     for factor_of_i in DICT_OF_FACTORS[i]:
         if (factor_of_i in DICT_OF_FACTORS[n]):
@@ -63,34 +79,16 @@ def is_coprime(i, n): ### Returns True if 'i' is cleanly divisible by any factor
 
     return True
 
-
-
-def factors(i):        # Accepts int, yields all factors of 'i', including 1 and 'i'
-    result = set()
-    sqrt_i = i**0.5 ### Could be a float
-
-    # for x in range(1, int(sqrt_i + 1)):
-    for x in range(2, int(sqrt_i + 1)): ### Don't care about 1
-        if (i % x == 0):
-
-            result.add(x)
-
-            if (x != sqrt_i): ### Only add matching factor if not square root
-                result.add(int(i / x))
-
-    return result
-
-
 ###################################################################
 ### "MAIN"
 ### Best times!!!
-# LIMIT = 10         ### 0m00.013s           ### Answer = 6
+LIMIT = 10         ### 0m00.013s           ### Answer = 6
 # LIMIT = 50         ### 0m00.013s           ### Answer = 30
 # LIMIT = 100        ### 0m00.013s           ### Answer = 30
 # LIMIT = 500        ### 0m00.018s           ### Answer = 210
 # LIMIT = 1000      ### 0m00.028s           ### Answer = 210
 # LIMIT = 5000       ### 0m00.152            ### Answer = 2310
-LIMIT = 10000      ### 0m00.661s           ### Answer = 2310
+# LIMIT = 10000      ### 0m00.661s           ### Answer = 2310
 # LIMIT = 50000      ### 0m27.154s           ### Answer = 30030 (n/φ(n) = 5.2135)
 # LIMIT = 100000     ### 2m30.528s           ### Answer = 30030
 # LIMIT = 500000     ### 0m??.???s           ### Answer = ???
@@ -117,63 +115,45 @@ DICT_OF_FACTORS = {}
 
 ####################################################################
 ### BEGIN MAIN LOOP (n)
-for n in range(2, LIMIT + 1): ### "Naive", all numbers
-# for n in range(2, LIMIT + 1, 2): ### Even numbers only (Target N will never be odd/prime)
-# for n in range(10, LIMIT + 1, 10): ### Kind of cheating, incrementing based on previous answers
-# for n in range(30, LIMIT + 1, 30): ### Kind of cheating, incrementing based on previous answers
-# for n in range(100000, LIMIT + 1, 30):
+for n in range(2, LIMIT + 1): ### "Naive", every number. Required for DICT_OF_FACTORS to be effective
 
     # print(f"\n==> (n = {n})")
-
-    ### Get primes of >= n
-    # primes_including_n = [i for i in yield_primes(n)]
-    
-
     
     ####################################################################
-    ### Find n/φ(n) 
-    # print(f"==> totient({n})")
-
-    # relative_primes = {1}
+    ### BEGIN "Find n/φ(n)" LOOP (i) 
     totient = 1
     current_n_on_tn = 0
-    factors_of_n = factors(n)
-    DICT_OF_FACTORS.update({ n:factors_of_n })
+    # factors_of_n = factors(n)
+    # DICT_OF_FACTORS.update({ n:factors_of_n })
     # print(f"Factors of {n} = {factors_of_n}")
 
-    # for i in range(2, n): ### Naive, all numbers
-    for i in range(3, n, 2): ### Odd numbers only (coprimes cannot be even)
+    for i in range(2, n): ### Naive, all numbers
+    # for i in range(3, n, 2): ### Odd numbers only (coprimes cannot be even)
 
-        # if (is_coprime_old(i, n)):
-        # if (
-        #     (abs(i - n) == 1) ### Consecutive numbers are always coprime
-        #     or (n % i == 0) ### Account for not including 'a' as a factor of itself below (NOTE: Swapped around?)
-        #     or is_coprime(i, factors_of_n) ### Run slowest check to last
-        # ):
-        if is_coprime_old_v2(i, factors_of_n, n):
-        # if is_coprime(i, n):
-            # relative_primes.add(i)
+        if is_coprime_find_factors_first(i, n):
+        # if is_coprime_find_and_check_factors(i, n):
+        # if is_coprime_precompute_factors_of_n(i, factors_of_n, n):
+        # if is_coprime_precompute_all_factors(i, n):
             totient += 1
-
             current_n_on_tn = n / totient
             # print(f"==> totient({n}): n/φ(n) for current iteration = {current_n_on_tn}")
 
+            # if (current_n_on_tn == MAX_N_ON_TN): print(f"\n==> (n = {n}) matches current best n/φ(n) {MAX_N_ON_TN}")
             if (current_n_on_tn < MAX_N_ON_TN):
                 # print(f"==> totient({n}): n/φ(n) fell below MAX_N_ON_TN ({MAX_N_ON_TN})")
                 break
 
-    # print(f"==> totient({n}): Relative primes = {relative_primes}")
+    ### END "Find n/φ(n)" LOOP (i) 
     ####################################################################
 
     # current_n_on_tn = n/totient(n)
-    # print(f"(n = {n}) current_n_on_tn = {current_n_on_tn}")
+    print(f"\n==> (n = {n}) φ({n}) = {totient}")
 
     if (current_n_on_tn > MAX_N_ON_TN):
         RESULT = n
         MAX_N_ON_TN = current_n_on_tn
 
         print(f"==> (n = {n}) New highest n/φ(n) found: {MAX_N_ON_TN}")
-
 
 ### END MAIN LOOP (n)
 ####################################################################
