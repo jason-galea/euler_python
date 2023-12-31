864ad2cc2ce57906126cf94a6f58a9fb5d0c098f
Jason Galea <jasonpgalea@outlook.com>
2023-12-31 18:23:38 +1100

    feat: I give up on 26

--- 26_not_fucking_braindead.py
+++ 26_not_fucking_braindead.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
 
-def main():
-    l = generate_decimals(7)
+MAX_DECIMALS = 12
 
-    print(f"{l=}")
+def main(max_range):
+
+    # for i in range(max_range):
+    for i in [7]:
+        decimals = generate_decimals(i)
+
+        print()
+        print(f"==> DEBUG: decimals = {decimals}")
 
 
 def generate_decimals(divisor):
     """Perform long division, return decimals"""
+
     decimals = [0]
-    max_result = 10
-
     dividend = 10 ### 1, but shifted left
-
     temp_dividend = dividend
 
-    for _ in range(max_result):
+    for _ in range(MAX_DECIMALS):
 
         dividend_over_divisor = int(temp_dividend / divisor) ### Drop remainder
         decimals.append(dividend_over_divisor)
-
         result_x_divisor = dividend_over_divisor * divisor
 
         ### Overwrite var for next loop
         temp_dividend = (temp_dividend - result_x_divisor) * 10
 
-        print()
-        print(f"==> DEBUG: dividend = {dividend}")
-        print(f"==> DEBUG: dividend_over_divisor = {dividend_over_divisor}")
-        print(f"==> DEBUG: result_x_divisor = {result_x_divisor}")
-        print(f"==> DEBUG: temp_dividend = {temp_dividend}")
+        ### Actually test for previous
+        if dividend_over_divisor in decimals[:-1]:
+            print(f"==> DEBUG: Found duplicate decimal '{dividend_over_divisor}'")
+            # print(f"==> DEBUG: decimals = {decimals}")
 
-    # next_dividend_over_divisor = int( dividend_minus_result / divisor )
-    # decimals.append(next_dividend_over_divisor)
-
-    # next_result_x_divisor = next_dividend_over_divisor * divisor
-
-    # print(f"==> DEBUG: next_dividend_over_divisor = {next_dividend_over_divisor}")
-    # print(f"==> DEBUG: next_result_x_divisor = {next_result_x_divisor}")
-
-
+            ### Test for multi-char divisors
+            ### NOTE: Loop over decimals list backwards, E.G decimals[-1:], decimals[-2:]
+            for i in range(len(decimals))
 
     return decimals
 
 
 if __name__ == "__main__":
-    main()
+    main(10)
