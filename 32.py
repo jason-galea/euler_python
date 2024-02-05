e6f709bccb611a3f8010fc5b43e7e1655ac7db82
Jason G <jasongalea98@outlook.com>
2024-02-05 22:42:37 +1100

    fun with recursion!

--- 32.py
+++ 32.py
@@ -19,12 +19,56 @@ def is_pandigital(i):
     return i_set == set(range(1, i_len + 1 )) ### E.G: {3,1,2} == {1,2,3}
 
 
-def main_single_integers():
-    LIMIT = 10000
+def get_all_combinations(n):
+    result = set()
+    complete_set = list(range(1, n + 1)) ### [1, 2, 3]
 
-    for i in range(LIMIT):
-        if is_pandigital(i):
-            print(f"Success with i = {i}")
+    for i in complete_set:
+        subset_1 = [] ### [?, ?, ?]
+        subset_1.append(i) ### [1, ?, ?]
+        print(f"{subset_1=}")
+
+        # if...
+
+        for j in subset_1:
+            subset_2 = subset_1 ### [1, ?, ?]
+            subset_2.append(j) ### [1, 2, ?]
+            print(f"{subset_2=}")
+
+            # if...
+
+            for k in subset_2:
+                subset_3 = subset_2 ### [1, 2, ?]
+                subset_3.append(k) ### [1, 2, 3]
+                print(f"{subset_3=}")
+
+                if len(subset_3) == n:
+                    result.update(subset_3)
+
+    return result
+
+
+def main_single_integers():
+    # LIMIT = 100000
+    LIMIT = 10**5
+
+    # ### Check if pandigital per int
+    # for i in range(LIMIT):
+    #     if is_pandigital(i):
+    #         print(f"Success with i = {i}")
+    #         pass
+
+    # ### Pre-calculate all pandigital numbers
+    # pandigital_len_limit = len(str(LIMIT)) - 1 ### E.G For 1000, longest pandigital is 3 digits
+
+    # pandigital_numbers = {1}
+    # for i in range(2, pandigital_len_limit + 1):
+    #     pandigital_numbers.update(get_all_combinations(i))
+
+    # print(f"{pandigital_numbers=}")
+
+    all_combinations_of_int = get_all_combinations(3)
+    print(f"{all_combinations_of_int=}")
 
 
 def main():
