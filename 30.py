916efab60802360370c58f938aa65df765ef3d1c
Jason G <jasongalea98@outlook.com>
2024-02-05 21:11:52 +1100

    feat: 30 going suspiciously well...

--- 30.py
+++ 30.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def digitize(n, base=10):
     """
     https://stackoverflow.com/questions/1906717/how-to-split-an-integer-into-a-list-of-digits
@@ -13,8 +12,11 @@ def digitize(n, base=10):
 
 
 def main():
-    LIMIT = 10000
+    LIMIT = 10**6
     TARGET_POWER = 5
+    SUCCESS_LIST = []
+
+    print(f"Limit = {LIMIT}")
 
     for i in range(2, LIMIT):
         i_digits = list(digitize(i))
@@ -30,11 +32,14 @@ def main():
         # print(f"{total_powers_of_i_digits=}")
 
         if i == total_powers_of_i_digits:
-            print(f"Success with i={i}")
+            print(f"Success with i = {i}")
             # print(f"{i=}")
             # print(f"{i_digits=}")
             # print(f"{total_powers_of_i_digits=}")
             # print()
+            SUCCESS_LIST.append(i)
+
+    print(f"Result = {sum(SUCCESS_LIST)}")
 
 
 if __name__ == "__main__":
