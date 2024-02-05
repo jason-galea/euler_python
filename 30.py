617bb09c0f5efa74d469f923719d902c72901edf
Jason G <jasongalea98@outlook.com>
2024-02-05 21:03:44 +1100

    feat: 30 going suspiciously well...

--- 30.py
+++ 30.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
 
+
+def digitize(n, base=10):
+    """
+    https://stackoverflow.com/questions/1906717/how-to-split-an-integer-into-a-list-of-digits
+    """
+    if n == 0:
+        yield 0
+    while n:
+        n, d = divmod(n, base)
+        yield d
+
+
 def main():
-    LIMIT = 1000
-    TARGET_POWER = 3
+    LIMIT = 10000
+    TARGET_POWER = 5
 
     for i in range(2, LIMIT):
+        i_digits = list(digitize(i))
+        i_digits.reverse()
 
-        i_digits = list(str(i))
+        # print(f"{i=}")
+        # print(f"{i_digits=}")
 
         total_powers_of_i_digits = 0
         for j in i_digits:
             total_powers_of_i_digits += j**TARGET_POWER
 
-        print(f"{i=}")
-        print(f"{total_powers_of_i_digits=}")
+        # print(f"{total_powers_of_i_digits=}")
 
         if i == total_powers_of_i_digits:
-            print("SUCCESS")
+            print(f"Success with i={i}")
+            # print(f"{i=}")
+            # print(f"{i_digits=}")
+            # print(f"{total_powers_of_i_digits=}")
+            # print()
 
 
 if __name__ == "__main__":
