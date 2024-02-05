dbf55f9dda9192a7b423a308e8e732ff28aa6237
Jason G <jasongalea98@outlook.com>
2024-02-05 20:44:30 +1100

    30 again lol

--- 30.py
+++ 30.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 
-LIMIT = 20
-
 def main():
-    for i in LIMIT:
+    LIMIT = 1000
+    TARGET_POWER = 3
 
-        ###
-        for j in range(1, 10+1):
-            for k in range(1, 10+1):
-                pass
+    for i in range(2, LIMIT):
+
+        i_digits = list(str(i))
+
+        total_powers_of_i_digits = 0
+        for j in i_digits:
+            total_powers_of_i_digits += j**TARGET_POWER
+
+        print(f"{i=}")
+        print(f"{total_powers_of_i_digits=}")
+
+        if i == total_powers_of_i_digits:
+            print("SUCCESS")
+
 
 if __name__ == "__main__":
     main()
